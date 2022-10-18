d5c49a7d5b944efa5cba6d129a462ec30d755f18
Ultimitepower36 <113638764+Ultimitepower36@users.noreply.github.com>
2022-10-18 14:34:11 -0600

    Introduction

--- Program.cs
+++ Program.cs
@@ -3,6 +3,10 @@
     JumpMan Jumper = new JumpMan();
 
     public void Game(){
+        Console.WriteLine("Welcome to Jumper!");
+        Console.WriteLine("Correctly guess the word within 4 guesses to save the Jumper!");
+        Console.WriteLine("");
+
         
 
     }

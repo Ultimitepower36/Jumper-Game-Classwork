cc17adbe5fd7e82463938e8342686d6113df4613
Ultimitepower36 <113638764+Ultimitepower36@users.noreply.github.com>
2022-10-18 14:18:10 -0600

    Jumper

---
+++ JumpMan.cs
@@ -0,0 +1,32 @@
+public class JumpMan
+{
+    string Row1 = "    ______    ";
+    string Row2 = "   /______\   ";
+    string Row3 = "    \   /    ";
+    string Row4 = "     \ /     ";
+    string Row5 = "      O ";
+    string Row6 = "     /|\ ";
+    string Row7 = "     / \ ";
+     string Row8 = "^^^^^^^^^^^^^^";
+
+    public Jumper(int error)
+    {
+        if (error == 0) {
+            Console.WriteLine($"Row1");
+        }
+        if (error <= 1) {
+            Console.WriteLine($"Row2");
+        }
+        if (error <= 2) {
+            Console.WriteLine($"Row3");
+        }
+        if (error <= 2) {
+            Console.WriteLine($"Row4");
+        }
+        Console.WriteLine($"Row5");
+        Console.WriteLine($"Row6");
+        Console.WriteLine($"Row7");
+        Console.WriteLine($"Row8");
+        return;
+    }
+}

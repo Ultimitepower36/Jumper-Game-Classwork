fdef4afe98f9ada06b8b75eb2a39c30cd4fa812c
Ultimitepower36 <113638764+Ultimitepower36@users.noreply.github.com>
2022-10-18 15:02:19 -0600

    Working on it

--- Words.cs
+++ Words.cs
@@ -7,13 +7,25 @@ class Words{
     List<string> Done = new List<string>{"D", "O", "N", "E"};
     List<string> Options = new List<string>{"Word", "Mark", "Fill", "Star", "Done"};
 
-    public string WordSelection(){
+    public WordSelection(){
         Random rnd = new Random();
         int number = rnd. Next(0, 5);
         var Selected = Options[number];
         List<string> Chosen = new List<string>{};
         if (Selected == "Word"){
             Chosen = Word;
+        }
+        if (Selected == "Mark"){
+            Chosen = Mark;
+        }
+        if (Selected == "Fill"){
+            Chosen = Fill;
+        }
+        if (Selected == "Star"){
+            Chosen = Star;
+        }
+        if (Selected == "Done"){
+            Chosen = Done;
         }
 
         return Chosen;

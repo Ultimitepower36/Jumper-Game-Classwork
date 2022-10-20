94ff5546ea525ccc8ab8c2292cf6b30e64ed1282
Ultimitepower36 <113638764+Ultimitepower36@users.noreply.github.com>
2022-10-20 14:15:02 -0600

    Main update

--- Program.cs
+++ Program.cs
@@ -1,14 +1,19 @@
 ﻿class jumperManGame{
 
-    JumpMan Jumper = new JumpMan();
+    JumpMan JumpMan = new JumpMan();
+    Words Words = new Words();
 
     public void Game(){
         Console.WriteLine("Welcome to Jumper!");
         Console.WriteLine("Correctly guess the word within 4 guesses to save the Jumper!");
         Console.WriteLine("");
+        var GameOver = false;
+        var ChosenWord = Words.WordSelection();
 
-        
+        while (!GameOver){
+            JumpMan.Jumper(0);
+            Console.WriteLine("Guess a letter [a-z] (lowercase please): ");
 
+        }
     }
-
 }

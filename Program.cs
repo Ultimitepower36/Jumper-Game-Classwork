c39455bb60a99d8e33c65ecfc4605233476f83b4
Ultimitepower36 <113638764+Ultimitepower36@users.noreply.github.com>
2022-10-20 14:42:32 -0600

    Update

--- Program.cs
+++ Program.cs
@@ -2,18 +2,19 @@
 
     static JumpMan JumpMan = new JumpMan();
     static Words Words = new Words();
-
+    List<string> Empty = new List<string>{"_", "_", "_", "_"};
     public static void Main(){
         Console.WriteLine("Welcome to Jumper!");
         Console.WriteLine("Correctly guess the word within 4 guesses to save the Jumper!");
         Console.WriteLine("");
         var GameOver = false;
-        var ChosenWord = Words.WordSelection();
+        List<string> ChosenWord = Words.WordSelection();
 
         while (!GameOver){
             JumpMan.Jumper(0);
             Console.WriteLine("Guess a letter [a-z] (lowercase please): ");
             var letter = Console.ReadLine() ?? "";
+
 
             GameOver = true;
         }
